2e1a83421883f2569a9565c1c2596f08825ab63e
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-04-06 18:35:36 +0300

    ToDoApp-with-React-Native

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import { appStyles as styles } from "./styles";
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar style="dark" />
       <View style={styles.container}>
-        <Text>My Tasks</Text>
-        <Text>
+        <Text style={styles.title}>My Tasks</Text>
+        <Text style={styles.subtitle}>
           Enter your tasks in the text box below and press the Add button to add{" "}
         </Text>
-        <StatusBar style="dark" />
+        <TextInput style={styles.input} placeholder="Enter your task here" />
+        <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
--- styles.js
+++ styles.js
@@ -1,3 +1,29 @@
 import { StyleSheet } from "react-native";
 
-export const appStyles = StyleSheet.create({});
+export const appStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#333",
+    marginTop: 8,
+  },
+  input: {
+    height: 40,
+    marginTop: 8,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    margin: 10,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    fontSize: 16,
+  },
+});
